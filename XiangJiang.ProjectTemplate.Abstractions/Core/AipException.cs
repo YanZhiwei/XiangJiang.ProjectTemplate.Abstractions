969323cb0ef14e802c151646b0575c98de0d27fb
YanZhiwei <Yan.Zhiwei@hotmail.com>
2021-03-28 23:23:24 +0800

    增加ApiServiceBase (#2)
    * 增加ApiServiceBase.cs
    
    * fix build
    
    * fix build error

---
+++ XiangJiang.ProjectTemplate.Abstractions/Core/AipException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace XiangJiang.ProjectTemplate.Abstractions.Core
+{
+    [Serializable]
+    public class AipException : Exception
+    {
+        public AipException()
+        {
+            Code = -1;
+        }
+
+        public AipException(string message)
+            : base(message)
+        {
+        }
+
+        public AipException(int code, string message)
+            : base(message)
+        {
+            Code = code;
+        }
+
+        public int Code { get; set; }
+    }
+}
---
+++ XiangJiang.ProjectTemplate.Abstractions/Core/ApiServiceBase.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using XiangJiang.ProjectTemplate.Abstractions.Result;
+
+namespace XiangJiang.ProjectTemplate.Abstractions.Core
+{
+    public abstract class ApiServiceBase
+    {
+        protected readonly object SyncObject = new object();
+        protected volatile bool HasDoneAuthoried;
+
+        protected ApiServiceBase(string apiKey, string secretKey)
+            : this("", apiKey, secretKey, false)
+        {
+        }
+
+        protected ApiServiceBase(string appId, string apiKey, string secretKey, bool debug)
+        {
+            AppId = appId;
+            ApiKey = apiKey;
+            SecretKey = secretKey;
+            ExpireAt = DateTime.Now;
+            Debug = debug;
+        }
+
+        protected string Token { get; set; }
+
+        protected DateTime ExpireAt { get; set; }
+
+        protected string AppId { get; set; }
+
+        protected string ApiKey { get; set; }
+
+        protected string SecretKey { get; set; }
+
+        protected bool Debug { get; set; }
+
+        protected abstract void DoAuthorization();
+
+        protected virtual bool NeedAuth()
+        {
+            if (!HasDoneAuthoried) return true;
+            return false;
+        }
+
+
+        protected void PreAction()
+        {
+            DoAuthorization();
+        }
+
+        protected T DoAction<T>(ApiResult<T> apiResult) where T : class, new()
+        {
+            if (!apiResult.IsSuccessful) throw new AipException(apiResult.StatusCode, apiResult.Message);
+
+            return apiResult.Data;
+        }
+    }
+}
